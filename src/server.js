15b1bf6d85127222d3535d96c63849417b89e998
Zian Waqas Javed <zianjaved@hotmail.com>
2022-03-07 12:04:29 +0000

    created multiple pages and linked them

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 5000;
 
 app.use("/static", express.static("public"));
+app.use("/static/page2.html", express.static("public"));
+app.use("/static/page3.html", express.static("public"));
 
 app.listen(port,() => {
     console.log(`Listening on port ${port}`)
